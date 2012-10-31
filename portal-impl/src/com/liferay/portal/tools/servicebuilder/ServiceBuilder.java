8a830f341c460785a4b19910db7ff41e5e9c8ea1
Miles Huang <miles.huang@unisystechconsulting.com>
2012-10-31 12:51:18 -0400

    ServiceBuilder Enhancement: Add treeScopeId to support multiple independent trees in a single company
    Conflicts:
    	portal-impl/src/com/liferay/portal/tools/servicebuilder/dependencies/persistence_impl.ftl

--- portal-impl/src/com/liferay/portal/tools/servicebuilder/ServiceBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/servicebuilder/ServiceBuilder.java
@@ -456,6 +456,9 @@ public class ServiceBuilder {
 			"(?m)^[ \t]*((?:package|import) .*;)\\s*^[ \t]*/\\*\\*",
 			"$1\n\n/**");
 
+		if ( newContent.length() == 0 ) {
+			System.exit(1);
+		}
 		/*
 		// Remove blank lines after try {
 
@@ -747,6 +750,10 @@ public class ServiceBuilder {
 								_createPersistenceTest(entity);
 							}
 
+							if ( entity.isHierarchicalTree() && entity.hasColumn("treeScopeId")) {
+								entity.getColumn("treeScopeId").setFinderPath(true);
+							}
+							
 							_createModelImpl(entity);
 							_createExtendedModelBaseImpl(entity);
 							_createExtendedModelImpl(entity);
