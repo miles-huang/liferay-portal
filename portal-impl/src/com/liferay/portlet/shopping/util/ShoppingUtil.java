b959cbde5ed56871d59a20dc982ff66280734d45
Miles Huang <miles.huang@live.com>
2015-05-13 10:37:17 -0400

    disable shopping portlet state condition for tax calculation since it isn't working.

--- portal-impl/src/com/liferay/portlet/shopping/util/ShoppingUtil.java
+++ portal-impl/src/com/liferay/portlet/shopping/util/ShoppingUtil.java
@@ -473,8 +473,7 @@ public class ShoppingUtil {
 			}
 		}
 
-		if ((preferences != null) &&
-			preferences.getTaxState().equals(stateId)) {
+		if ((preferences != null) ) {
 
 			double subtotal = 0.0;
 
